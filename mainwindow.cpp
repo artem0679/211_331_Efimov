ed08824740857f3d965daf450264ec84e9182552
artem0679 <artem0679@bk.ru>
2025-10-04 14:17:25 +0300

    1ый блок сделан

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -5,7 +5,10 @@
 #include <QIODevice>
 #include <QFile>
 #include <QInputDialog>
-
+#include <QMessageBox>
+#include <QApplication>
+#include <QRegularExpression>
+#include <QString>
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
@@ -23,20 +26,37 @@ MainWindow::MainWindow(QWidget *parent)
 
 bool MainWindow::verifyPin()
 {
+    QString storedPinHash;
+    QFile hashFile(":/data/pin_hash.txt");
+    if (!hashFile.open(QIODevice::ReadOnly | QIODevice::Text)) {
+        QMessageBox::critical(this, "Ошибка", "Не удалось открыть файл с хешем ПИН-кода (:/pin_hash.txt).");
+        return false;
+    }
+    QTextStream in(&hashFile);
+    storedPinHash = in.readLine().trimmed(); // Читаем первую строку и убираем пробелы
+    hashFile.close();
+
+
+    // ввод пинкода пользователем
     QString enteredPin = QInputDialog::getText(this, tr("ПИН-код"), tr("Введите ПИН-код:"), QLineEdit::Password);
 
+
     if (enteredPin.isEmpty()) {
-        return false; // Отменили ввод
+        return false;
     }
 
+    // вычисление хэша от пинкода-
     QByteArray pinBytes = enteredPin.toUtf8();
     QByteArray hashBytes = QCryptographicHash::hash(pinBytes, QCryptographicHash::Md5);
     QString enteredPinHash = hashBytes.toHex();
 
-    // Хеш от "1234"
-    const QString STORED_PIN_HASH = "81dc9bdb52d04dc20036dbd8313ed055";
-
-    return enteredPinHash == STORED_PIN_HASH;
+    // Сравниваем вычисленный хеш с хешем из файла ресурсов
+    if (enteredPinHash == storedPinHash) {
+        return true;
+    } else {
+        QMessageBox::warning(this, "Ошибка", "Неверный ПИН");
+        return false;
+    }
 }
 
 void MainWindow::loadDataAndDisplay(const QString &fileName)
