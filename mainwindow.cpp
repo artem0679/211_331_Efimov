97b0788b3a28ae1f361ecb47e0bad56c06d5e3e7
artem0679 <artem0679@bk.ru>
2025-10-04 14:09:47 +0300

    Hard Pin (fix problems 1.3)

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -4,6 +4,7 @@
 #include <QMessageBox>
 #include <QIODevice>
 #include <QFile>
+#include <QInputDialog>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -11,11 +12,31 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
     textEditData = ui->textEditData;
-    if (!textEditData) {
-        QMessageBox::critical(this, "Error", "Could not find textEditData in UI file.");
+    if (!verifyPin()) {
+        QMessageBox::warning(this, "Доступ запрещён", "Неверный ПИН. Приложение закрывается.");
+        QCoreApplication::quit(); // Закрываем приложение полностью
         return;
     }
+
     loadDataAndDisplay(":/data/data.csv"); // Загрузим данные при старте
+}
+
+bool MainWindow::verifyPin()
+{
+    QString enteredPin = QInputDialog::getText(this, tr("ПИН-код"), tr("Введите ПИН-код:"), QLineEdit::Password);
+
+    if (enteredPin.isEmpty()) {
+        return false; // Отменили ввод
+    }
+
+    QByteArray pinBytes = enteredPin.toUtf8();
+    QByteArray hashBytes = QCryptographicHash::hash(pinBytes, QCryptographicHash::Md5);
+    QString enteredPinHash = hashBytes.toHex();
+
+    // Хеш от "1234"
+    const QString STORED_PIN_HASH = "81dc9bdb52d04dc20036dbd8313ed055";
+
+    return enteredPinHash == STORED_PIN_HASH;
 }
 
 void MainWindow::loadDataAndDisplay(const QString &fileName)
--- mainwindow.h
+++ mainwindow.h
@@ -3,11 +3,10 @@
 
 #include <QMainWindow>
 #include <QTextEdit>
+#include <QCryptographicHash>
 
 QT_BEGIN_NAMESPACE
-namespace Ui {
-class MainWindow;
-}
+namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
 
 class MainWindow : public QMainWindow
@@ -21,6 +20,9 @@ public:
 private:
     Ui::MainWindow *ui;
     QTextEdit *textEditData;
+
     void loadDataAndDisplay(const QString &fileName);
+
+    bool verifyPin();
 };
 #endif // MAINWINDOW_H
